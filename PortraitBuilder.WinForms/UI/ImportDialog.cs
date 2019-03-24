e89e5959ffbef8c70e3c326d0e7dda932cd37851
J. Zebedee <zebedee@code.gripe>
2019-03-24 03:38:53 -0500

    Clean up ImportDialog

--- PortraitBuilder.WinForms/UI/ImportDialog.cs
+++ PortraitBuilder.WinForms/UI/ImportDialog.cs
@@ -1,72 +1,89 @@
 ﻿using PortraitBuilder.Model.Portrait;
 using System;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
-namespace PortraitBuilder.UI {
-	public partial class ImportDialog : Form {
-		private bool isDNAValid = false; 
-		private bool isPropertiesValid = false;
+namespace PortraitBuilder.UI
+{
+    public partial class ImportDialog : Form
+    {
+        private bool isDNAValid = false;
+        private bool isPropertiesValid = false;
 
-		public Portrait portrait = new Portrait();
+        public Portrait portrait = new Portrait();
 
-		public ImportDialog() {
-			InitializeComponent();
-		}
+        public ImportDialog()
+        {
+            InitializeComponent();
+        }
 
-		private void btnCancel_Click(object sender, EventArgs e) {
-			Close();
-		}
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
 
-		private void btnOK_Click(object sender, EventArgs e) {
-			string dna = tbDNA.Text;
-			string properties = tbProperties.Text;
-			portrait.import(dna, properties);
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            portrait.DNA = tbDNA.Text;
+            portrait.Properties = tbProperties.Text;
 
-			DialogResult = DialogResult.OK;
-			Close();
-		}
+            DialogResult = DialogResult.OK;
+            Close();
+        }
 
-		private void tb_TextChanged(object sender, EventArgs e) {
-			TextBox tb = (TextBox)sender;
+        private void tb_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
 
-			if (tb == tbDNA) {
-				isDNAValid = Validate(tb, Characteristic.DNA.Length);
-			} else if (tb == tbProperties) {
-				isPropertiesValid = Validate(tb, Characteristic.PROPERTIES.Length);
-			}
+            if (tb == tbDNA)
+            {
+                isDNAValid = Validate(tb, Characteristic.DNA.Length);
+            }
+            else if (tb == tbProperties)
+            {
+                isPropertiesValid = Validate(tb, Characteristic.PROPERTIES.Length);
+            }
 
-			if (isDNAValid && isPropertiesValid) {
-				btnOK.Enabled = true;
-			} else {
-				btnOK.Enabled = false;
-			}
-		}
+            if (isDNAValid && isPropertiesValid)
+            {
+                btnOK.Enabled = true;
+            }
+            else
+            {
+                btnOK.Enabled = false;
+            }
+        }
 
-		private bool Validate(TextBox tb, int length) {
-			bool isValid = IsValid(tb.Text, length);
-			if (isValid) {
-				errorProvider.SetError(tb, string.Empty);
-			} else {
-				errorProvider.SetError(tb, "Invalid text.");
-			}
-			return isValid;
-		}
+        private bool Validate(TextBox tb, int length)
+        {
+            bool isValid = IsValid(tb.Text, length);
+            if (isValid)
+            {
+                errorProvider.SetError(tb, string.Empty);
+            }
+            else
+            {
+                errorProvider.SetError(tb, "Invalid text.");
+            }
+            return isValid;
+        }
 
-		private bool IsValid(string dnaOrProperties, int length) {
-			bool valid = true;
+        private bool IsValid(string dnaOrProperties, int length)
+        {
+            bool valid = true;
 
-			foreach (char c in dnaOrProperties) {
-				if (!Char.IsLetterOrDigit(c)) {
-					valid = false;
-					break;
-				}
-			}
+            foreach (char c in dnaOrProperties)
+            {
+                if (!Char.IsLetterOrDigit(c))
+                {
+                    valid = false;
+                    break;
+                }
+            }
 
-			if (dnaOrProperties.Length != length)
-				valid = false;
+            if (dnaOrProperties.Length != length)
+                valid = false;
 
-			return valid;
-		}
-	}
+            return valid;
+        }
+    }
 }
