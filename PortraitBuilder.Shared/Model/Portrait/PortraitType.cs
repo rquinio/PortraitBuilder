3ab3e93e743e29e09eb098ec204fac3138b1cacb
J. Zebedee <zebedee@code.gripe>
2019-03-24 03:00:59 -0500

    Clean up PortraitType

--- PortraitBuilder.Shared/Model/Portrait/PortraitType.cs
+++ PortraitBuilder.Shared/Model/Portrait/PortraitType.cs
@@ -1,77 +1,65 @@
 using System.Collections.Generic;
+using System.Linq;
 
-namespace PortraitBuilder.Model.Portrait {
+namespace PortraitBuilder.Model.Portrait
+{
 
-	/// <summary>
-	/// Definition of how a portrait for a given ethnicity, sex, age and date is composed.
-	/// </summary>
-	public class PortraitType {
+    /// <summary>
+    /// Definition of how a portrait for a given ethnicity, sex, age and date is composed.
+    /// </summary>
+    public class PortraitType
+    {
 
-		/// <summary>
-		/// E.g. PORTRAIT_westerngfx_male
-		/// </summary>
-		public string Name;
+        /// <summary>
+        /// E.g. PORTRAIT_westerngfx_male
+        /// </summary>
+        public string Name { get; set; }
 
-		public string EffectFile;
+        public string EffectFile { get; set; }
 
-		/// <summary>
-		/// Letter index of the hair colour in the DNA string
-		/// </summary>
-		public int HairColourIndex = Characteristic.HAIR_COLOR.index;
+        /// <summary>
+        /// Letter index of the hair colour in the DNA string
+        /// </summary>
+        public int HairColourIndex { get; set; } = Characteristic.HAIR_COLOR.index;
 
-		/// <summary>
-		/// Letter index of the eye colour in the DNA string
-		/// </summary>
-		public int EyeColourIndex = Characteristic.EYE_COLOR.index;
+        /// <summary>
+        /// Letter index of the eye colour in the DNA string
+        /// </summary>
+        public int EyeColourIndex { get; set; } = Characteristic.EYE_COLOR.index;
 
-		/// <summary>
-		/// List of layers composing the portraitType definition
-		/// </summary>
-		public List<Layer> Layers = new List<Layer>();
+        /// <summary>
+        /// List of layers composing the portraitType definition
+        /// </summary>
+        public List<Layer> Layers { get; set; } = new List<Layer>();
 
-		public List<Hair> HairColours = new List<Hair>();
-		public List<Colour> EyeColours = new List<Colour>();
+        public List<Hair> HairColours { get; set; } = new List<Hair>();
+        public List<Colour> EyeColours { get; set; } = new List<Colour>();
 
-		/// <summary>
-		/// TODO How is this used in-game ?
-		/// </summary>
-		public List<int> HeadgearThatHidesHair = new List<int>();
+        /// <summary>
+        /// TODO How is this used in-game ?
+        /// </summary>
+        public List<int> HeadgearThatHidesHair { get; set; } = new List<int>();
 
-		/// <summary>
-		/// The file that the data was loaded from (E.g portraits.gfx)
-		/// </summary>
-		public string Filename;
+        /// <summary>
+        /// The file that the data was loaded from (E.g portraits.gfx)
+        /// </summary>
+        public string Filename { get; set; }
 
-		public List<Characteristic> getCustomCharacterstics() {
-			List<Characteristic> customCharacteristics = new List<Characteristic>();
-			foreach (Layer layer in this.Layers) {
-				if (layer.Characteristic.custom == true) {
-					customCharacteristics.Add(layer.Characteristic);
-				}
-			}
-			return customCharacteristics;
-		}
+        public IEnumerable<Characteristic> CustomCharacteristics
+            => Layers.Select(l => l.Characteristic).Where(c => c.custom);
 
-		public override string ToString() {
-			return string.Format("Name: {0}, Layers: {1}, HairColours: {2}, EyeColours: {3}", Name, Layers.Count, HairColours.Count, EyeColours.Count);
-		}
+        public override string ToString()
+            => $"Name: {Name}, Layers: {Layers.Count}, HairColours: {HairColours.Count}, EyeColours: {EyeColours.Count}";
 
-		/// <summary>
-		/// Whether this portraitType is a base portraitType. 
-		/// 
-		/// Base portraitTypes can also be used as override portraitTypes, with culture-indexed layers replacing the ones of another base.
-		/// </summary>
-		public bool IsBasePortraitType(){
-			return HairColours.Count > 0;
-		}
+        /// <summary>
+        /// Whether this portraitType is a base portraitType. 
+        /// 
+        /// Base portraitTypes can also be used as override portraitTypes, with culture-indexed layers replacing the ones of another base.
+        /// </summary>
+        public bool IsBasePortraitType()
+            => HairColours.Any();
 
-		public Layer GetCultureLayer(int cultureIndex) {
-			foreach (Layer layer in this.Layers) {
-				if (layer.CultureIndex == cultureIndex) {
-					return layer;
-				}
-			}
-			return null;
-		}
-	}
+        public Layer GetCultureLayer(int cultureIndex)
+            => Layers.FirstOrDefault(l => l.CultureIndex == cultureIndex);
+    }
 }
