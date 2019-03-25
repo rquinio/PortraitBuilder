73f458d7af807d3cddc11101cbe6c84722c5427c
J. Zebedee <zebedee@code.gripe>
2019-03-24 19:28:08 -0500

    Add more to online portrait

--- PortraitBuilder.Online/Portrait.cs
+++ PortraitBuilder.Online/Portrait.cs
@@ -1,15 +1,16 @@
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using PortraitBuilder.Model.Portrait;
+using PortraitBuilder.Engine;
+using PortraitBuilder.Model;
 
 namespace PortraitBuilder.Online
 {
-    public static class Function1
+    public static class PortraitFunction
     {
         [FunctionName("portrait")]
         public static async Task<IActionResult> Run(
@@ -18,27 +19,36 @@ namespace PortraitBuilder.Online
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string dna = req.Query["dna"];
+            string properties = req.Query["properties"];
+            if (string.IsNullOrEmpty(dna) || string.IsNullOrEmpty(properties))
+                return new BadRequestResult();
 
-            //string dna = req.Query["dna"];
-            //string properties = req.Query["properties"];
+            string customProperties = req.Query["customProperties"];
 
-            //updatePortrait(dialog.portrait.DNA, dialog.portrait.Properties, "");
+            var portrait = new Portrait();
+            portrait.Import(dna, properties + customProperties);
 
-            //// Reflect on dropdown
+            // Reflect on dropdown
             //updateSelectedCharacteristicValues(portrait);
 
             //started = true;
 
-            //drawPortrait();
+            var portraitRenderer = new PortraitRenderer();
+            User user = new User
+            {
+                GameDir = @"X:\Games\Steam\steamapps\common\Crusader Kings II",//readGameDir();
+                ModDir = @"C:\Users\scorp\Documents\Paradox Interactive\Crusader Kings II",//readModDir(user.GameDir);
+                DlcDir = "dlc/"
+            };
+            var loader = new Loader(user);
+            var bmp = portraitRenderer.DrawPortrait(portrait, loader.ActiveContents, loader.ActivePortraitData.Sprites);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new OkObjectResult(bmp);
         }
     }
 }
